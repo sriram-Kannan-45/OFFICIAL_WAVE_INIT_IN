392d3e8595be22f3382aecf90f6f2a942d449284
SRIRAM K <wavene20@gmail.com>
2026-09-19 14:36:47 +0530

    feat: useLenis shared smooth scroll hook

---
+++ client/src/components/experience/useLenis.js
@@ -0,0 +1,32 @@
+import { useEffect } from 'react'
+import Lenis from 'lenis'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+
+gsap.registerPlugin(ScrollTrigger)
+
+let lenisInstance = null
+
+export function getLenis() {
+  return lenisInstance
+}
+
+export default function useLenis() {
+  useEffect(() => {
+    const reduce = window.matchMedia('(prefers-reduced-motion: reduce)').matches
+    if (reduce) return
+
+    const lenis = new Lenis({ duration: 1.1, smoothWheel: true })
+    lenisInstance = lenis
+
+    lenis.on('scroll', ScrollTrigger.update)
+    gsap.ticker.add((time) => lenis.raf(time * 1000))
+    gsap.ticker.lagSmoothing(0)
+
+    return () => {
+      gsap.ticker.remove(lenis.raf)
+      lenis.destroy()
+      lenisInstance = null
+    }
+  }, [])
+}
